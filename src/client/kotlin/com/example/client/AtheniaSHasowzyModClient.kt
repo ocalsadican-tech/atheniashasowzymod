93d0ad8ad2661218e81168d403d06cbd818131d4
ocalsadican-tech <ocalsadican@gmail.com>
2026-09-24 16:47:44 +0300

    Add files via upload

---
+++ src/client/kotlin/com/example/client/AtheniaSHasowzyModClient.kt
@@ -0,0 +1,9 @@
+package com.example.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object AtheniaSHasowzyModClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
