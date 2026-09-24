8674901fd52a14ab252343272f841e2db08e41c0
ocalsadican-tech <ocalsadican@gmail.com>
2026-09-24 16:48:56 +0300

    Delete src directory

--- src/client/kotlin/com/example/client/AtheniaSHasowzyModClient.kt
+++
@@ -1,9 +0,0 @@
-package com.example.client
-
-import net.fabricmc.api.ClientModInitializer
-
-object AtheniaSHasowzyModClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
--- src/client/kotlin/com/example/client/AtheniaSHasowzyModDataGenerator.kt
+++
@@ -1,9 +0,0 @@
-package com.example.client
-
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
-
-object AtheniaSHasowzyModDataGenerator : DataGeneratorEntrypoint {
-	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
-	}
-}
